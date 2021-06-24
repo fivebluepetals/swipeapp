ccd56945cba80e1028dca4e62e8fa0e8c921da0a
Valerie Wang <valerielle.wang@gmail.com>
2021-06-24 11:25:58 -0400

    add a lever object

--- _app/src/App.vue
+++ _app/src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <div id="foo">
-    Count: {{ counter }}
-  </div>
+  <svg id="app"
+       xmlns="http://www.w3.org/2000/svg">
+    <Lever />
+  </svg>
 </template>
 
 <script>
+import Lever from "./controls/Lever.vue"
+
 export default {
+  components: {
+    Lever
+  },
+
   data() {
     return {
-      counter: 10
+      groups: [
+        {},
+      ]
     }
   }
 }
 </script>
+
+<style scoped>
+svg {
+  width: 100%;
+  height: 100vh;
+  background-color: #eee;
+}
+</style>
